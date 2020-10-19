bd36ab7880078cdc66f6171926704fd4ca48b430
mirmidion43 <69776783+mirmidion43@users.noreply.github.com>
2020-10-19 15:57:33 +1100

    prep for 70%

--- Assets/Scripts/PacStudentController.cs
+++ Assets/Scripts/PacStudentController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PacmanMover : MonoBehaviour
+public class PacStudentController : MonoBehaviour
 {
    
     private Tween activeTween = null;
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -14,7 +14,7 @@ public class UIManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        text.SetActive(false);
     }
 
     // Update is called once per frame
